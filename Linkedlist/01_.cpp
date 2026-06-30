a186f5f01e52033b20ea97a5475018839eabc9c5
Shivam14raj <shivamrraj2005@gmail.com>
2026-06-30 11:20:44 +0530

    slow and fast pointer

--- Linkedlist/01_.cpp
+++ Linkedlist/01_.cpp
@@ -41,13 +41,46 @@ void PrintList(Node*head){
    cout << "NULL";
 } 
 
-// removing duplicates from the linkedist and printing 
+// removing duplicates from the linkedist(sorted) and printing 
+void removeDuplicates(int arr[], int size){
+    Node*head = new Node(arr[0]);
+    Node*temp = head; 
+    Node*next = head->next;  
+}
+
+
+// slow and fast pointer concept 
+// finding middle of the element 
+Node* middleELement(int arr[], int size){
+    Node*head = new Node(arr[0]);
+    
+    Node*temp = head; 
+    for(int i = 1; i < size; i++){
+       temp->next = new Node(arr[i]); 
+       temp = temp->next;
+    }
+
+    Node*slow = head; 
+    Node*fast = head; 
+    
+    while(fast!= NULL && fast->next != NULL){
+        slow = slow->next; // slow move by 1
+        fast = fast->next->next; // fast move by 2
+    } 
+
+      return slow; 
+    
+}
+
+
+// detecting cycle in a linkedlist / creating a cycle in a linkedlist  
+
 
 
 
 int main(){
 
-int arr[] = {1,2,3,4,5,5,6,7,8,9}; 
+int arr[] = {1,2,3,4,5,6,7,8,9}; 
 
 int size = sizeof(arr) / sizeof(arr[0]); 
 
@@ -56,6 +89,14 @@ int val = arr[0];
 Node* head = ArrayToLinkedList(arr, size);
 
 PrintList(head); 
+
+removeDuplicates(arr, size); 
+
+cout << endl; 
+
+Node*middle = middleELement(arr, size); 
+
+cout << "the middle element is: "<< middle->data; 
 
 return 0; 
 }
